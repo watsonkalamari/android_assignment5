d6056cc34c6622e3b7f0a59c60356f2231f5a228
watsonkalamari <klwatson3@ualr.edu>
2020-11-29 16:46:19 -0600

    Partially working/ Work in progress
    Can select emails.

---
+++ android-recyclerview-assignment-alt-master/app/src/main/java/com/ualr/recyclerviewassignment/InboxViewModel.java
@@ -0,0 +1,19 @@
+package com.ualr.recyclerviewassignment;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.ualr.recyclerviewassignment.model.Inbox;
+
+public class InboxViewModel extends ViewModel {
+    //TODO::set up the ViewModel class to be used to exchange the inbox data between the activity and the fragment
+
+    private final MutableLiveData<Inbox>selected = new MutableLiveData<Inbox>();
+    public void select(Inbox inbox){
+        selected.setValue(inbox);
+    }
+    public LiveData<Inbox>getSelected(){
+        return selected;
+    }
+}
--- android-recyclerview-assignment-alt-master/app/src/main/java/com/ualr/recyclerviewassignment/fragments/InboxListFragment.java
+++ android-recyclerview-assignment-alt-master/app/src/main/java/com/ualr/recyclerviewassignment/fragments/InboxListFragment.java
@@ -1,11 +1,9 @@
 package com.ualr.recyclerviewassignment.fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,7 +14,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.ualr.recyclerviewassignment.Adapter.AdapterListClass;
 import com.ualr.recyclerviewassignment.R;
 import com.ualr.recyclerviewassignment.Utils.DataGenerator;
-import com.ualr.recyclerviewassignment.databinding.ActivityListMultiSelectionBinding;
 import com.ualr.recyclerviewassignment.model.Inbox;
 import com.ualr.recyclerviewassignment.databinding.InboxListFragmentBinding;
 
@@ -37,12 +34,12 @@ public class InboxListFragment extends Fragment {
         return inflater.inflate(R.layout.inbox_list_fragment, container, false);
 
     }
+
     //TODO::Get the recyclerview to show up on the screen.
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
-    {
-       /* binding = InboxListFragmentBinding.inflate(getLayoutInflater());*/
-        RecyclerView recyclerView=view.findViewById(R.id.recyclerView);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        /* binding = InboxListFragmentBinding.inflate(getLayoutInflater());*/
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
         List<Inbox> items = DataGenerator.getInboxData(getActivity());
@@ -50,19 +47,24 @@ public class InboxListFragment extends Fragment {
         adapter = new AdapterListClass(getActivity(), DataSource);
         recyclerView.setAdapter(adapter);
 
-        /* items.addAll(DataGenerator.getInboxData(getActivity()));*/
-        /*items.addAll(DataGenerator.getInboxData(getActivity()));*/
-
-
-        /* adapter.setOnItemClickListener(this);
         adapter.setOnItemClickListener(new AdapterListClass.OnItemClickListener() {
             @Override
             public void onItemClick(int position) {
-                adapter.toggleItemState(position);*/
+                adapter.toggleItemState(position);
+            }
+
+
+            /* items.addAll(DataGenerator.getInboxData(getActivity()));*/
+            /*items.addAll(DataGenerator.getInboxData(getActivity()));*/
+
+
+            /* adapter.setOnItemClickListener(this);
+             */
 
        /* items.addAll(DataGenerator.getInboxData(getActivity()));
         items.addAll(DataGenerator.getInboxData(getActivity()));*/
 
+        });
     }
 }
 
